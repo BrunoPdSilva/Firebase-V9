5de324ce1327775a143d2fa385bc6f6b837d1d81
Bruno Peres <bruno_16515@hotmail.com>
2022-07-25 10:33:50 -0300

    Configurando firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzXdXjcgzFAclRBhTJ49OjpGUUSgIiDpc",
+  authDomain: "fir-v9-87a71.firebaseapp.com",
+  projectId: "fir-v9-87a71",
+  storageBucket: "fir-v9-87a71.appspot.com",
+  messagingSenderId: "383939013875",
+  appId: "1:383939013875:web:57a5219f4d1e3d4a595b86",
+};
+
+initializeApp(firebaseConfig);
